6fdf9d8d3efbb3a9045a1d0e0b84d1fe12cee781
Lauanny Nieuwenhoff Oliveira <126528019+nieuwenhof@users.noreply.github.com>
2024-09-05 10:46:02 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+print("""
+
+
+𝓝𝓲𝓮𝓾𝔀𝓮𝓷𝓱𝓸𝓯𝓯 𝓜𝓮𝓭𝓲𝓬𝓲𝓷𝓪
+
+      """)
+
+print('1. Cadastro dos alunos')
+print('2. Lista dos alunos')
+print('3. Ativar matricula dos estudantes')
+print('4. Sair da aplicação')
+
+opcao_escolhida = input('Escolha uma opção: ')
+print('Você escolheu a opção: ', opcao_escolhida)
